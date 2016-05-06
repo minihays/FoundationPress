e76ab62c4173f76be64b293db78c0ed651f89702
Andi Hays <github@andihays.net>
2016-05-05 22:50:36 -0500

    Lobster font to titles.

--- template-parts/content.php
+++ template-parts/content.php
@@ -12,7 +12,7 @@
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('blogpost-entry'); ?>>
 	<header>
-		<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<h2 class="blogpost-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		<?php foundationpress_entry_meta(); ?>
 	</header>
 	<div class="entry-content">
